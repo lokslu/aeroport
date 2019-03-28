ad38ea89d015fe4e6c61690853bd69e370b20625
PRO LoKsLu <sanya-melnik-2002@mail.ru>
2019-03-28 03:18:10 +0200

    Доавлено: возможность добовлять записи в файл

--- test/plane.cpp
+++ test/plane.cpp
@@ -2,13 +2,16 @@
 #include <iostream>
 #include <istream>
 #include <fstream>
-#include <sstream>
+#include <sstream> //для  stringstream  // для переобразавания float в string
 #include <string>
-#include <conio.h>
+#include <vector>
 #include <stdio.h>
+#include <windows.h>
 #include "plane.h" //обьявление класа
-#include "menu.h" // реализацыя мею
-#include <windows.h>	
+#include "menu.h" // реализацыя меню
+#include "dirent.h"
+#include <filesystem>
+
 
 #define KEY_UP 72 //по стандарту определяет что  KEY_UP -- 72
 #define KEY_DOWN 80
@@ -167,39 +170,31 @@ void plane::save_text(string neme_file)
 		file.close();
 	
 }
-void plane::save_text_dop()
+void plane::save_text_dop(string neme_file)
 {	
 	
-	fstream filename;
-	fstream file("./data/DFD", ios::app);
 	
-	file << endl << nomber_reis << " " << type_plenet << " " << point_plein << " " << time_vilet << " " << time_finish << endl;
+	fstream file(neme_file, ios::app);
+	
+	file << nomber_reis << " " << type_plenet << " " << point_plein << " " << time_vilet << " " << time_finish << endl;
 	file.close();
 
 }
-
-
 void plane::read_text()
 {
 }
-
-
 void plane::search_for_dest()
 {
 }
-
 void plane::vivod_cmd()
 {
-}
-
-	
+}	
 plane::~plane()
 {
 }
-
 void fynk_save(string*type_mas_planet, string*yes_no, string*zapis_data)
 {
-	int zapis,  gt, gd;
+	int zapis, gt, gd;
 
 	zapis = 1;
 
@@ -216,13 +211,13 @@ void fynk_save(string*type_mas_planet, string*yes_no, string*zapis_data)
 	do
 	{
 		gd = startMenuCycle(yes_no, 3);
-		
-		
+
+
 		if (gd == 1)
 		{
 			gd = 1;
 			if (0 == zapis % 2)
-			{
+			{	
 				zapis++;
 				numbers = new plane[zapis];
 				for (int j = 0; j < zapis - 1; j++)
@@ -254,23 +249,19 @@ void fynk_save(string*type_mas_planet, string*yes_no, string*zapis_data)
 
 
 	gt = startMenuCycle(zapis_data, 3);
-	
+
 	switch (gt)
 	{
 	case 1:
 	{
+		cout << "Введите имя файла" << endl;
+		string neme_file;
+		string neme_file1;
+		cin.get();
+		getline(cin, neme_file1);
+		neme_file.append("./data/" + neme_file1);
 		if (0 == zapis % 2)
 		{
-			cout << "Введите имя файла" << endl;
-			string neme_file1;
-			string neme_file;
-			cin.get();
-			getline(cin,neme_file1);
-			
-			neme_file.append("./data/"+neme_file1 );
-			
-	
-
 			for (int i = 0; i < zapis; i++)
 			{
 				numbers2[i].save_text(neme_file);
@@ -279,45 +270,90 @@ void fynk_save(string*type_mas_planet, string*yes_no, string*zapis_data)
 		}
 		else
 		{
-
-			cout << "Введите имя файла" << endl;
-			string neme_file;
-			string neme_file1;
-			cin.get();
-			getline(cin, neme_file1);
-			neme_file.append("./data/" + neme_file1);
-
 			for (int i = 0; i < zapis; i++)
 			{
-				
 				numbers[i].save_text(neme_file);
 			}
 			delete[] numbers;
 		}
-	}break;
+	}
+	break;
 	case 2:
 	{
-		if (0 == zapis % 2)
+		//***********************
+
+		vector<string> l;
+
+		//header('Content-Type: text/html; charset=utf-8'); //ПЕРЕКЛЮЧАЕТ ТИП ШИФРОВАНИЯ
+
+		namespace fs = std::experimental::filesystem;
 		{
-			for (int i = 0; i < zapis; i++)
+
+			stringstream ss; // для переобразавания float в string
+			string ff[10];
+			int h = 0;
+			fs::path p;
+			for (const auto & entry : fs::directory_iterator("./data"))
 			{
+				p = entry.path();
 				
-
-				numbers2[i].save_text_dop();
+				//ff[h]= p.string();
+				l.emplace_back(p.string());
+				//h++;
 			}
-			delete[] numbers2;
-		}
-		else
-		{
 
+		
 
-			for (int i = 0; i < zapis; i++)
+			int size_din_mas_menu= size(l) + 1;
+			string *lp = new string[size_din_mas_menu];
+			lp[0] = "Выбирете файл:";
+			h = 0;
+			for (int i = 1; i < size_din_mas_menu; i++)
 			{
-				numbers[i].save_text_dop();
+
+				lp[i] = l[h];
+				lp[i].erase(0, 7);
+				h++;
 			}
-			delete[] numbers;
+
+
+
+			int menunom;
+			 menunom = startMenuCycle(lp, size_din_mas_menu);
+			
+			string neme_file;
+
+			neme_file.append("./data/" + lp[menunom]);
+			delete[] lp;
+
+
+			//***********************
+
+
+			if (0 == zapis % 2)
+			{
+				for (int i = 0; i < zapis; i++)
+				{
+					numbers2[i].save_text_dop(neme_file);
+				}
+				delete[] numbers2;
+			}
+			else
+			{
+
+
+				for (int i = 0; i < zapis; i++)
+				{
+					numbers[i].save_text_dop(neme_file);
+				}
+				delete[] numbers;
+			}
+
 		}
-	}break;
+
+	}
+	break;
 	}
 
 }
+
--- test/plane.h
+++ test/plane.h
@@ -16,7 +16,7 @@ public:
 
 	void vvod_nap_klav(string*type_mas_planet);//Введення напрямку з клавіатури;	
 	void save_text(string neme_file);//Збереження у текстовому файлі;
-	void save_text_dop();
+	void save_text_dop(string neme_file);
 	void read_text();//Зчитування інформації з файлу та відбір рейсів у даному напрямку;	
 	void search_for_dest();//Пошук рейсів з мінімальним часом польоту у заданому напрямку;
 	void vivod_cmd();//Вивід у консольне вікно переліку рейсів у заданому напрямку за збільшенням часу польоту.
